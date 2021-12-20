f793eb523e3c05b47e778574f17bed6ad845dd0b
iwebnal <iwebnal@gmail.com>
2021-12-20 16:19:11 +0300

    Update response class for asserts

--- src/baseclasses/response_classes.py
+++ src/baseclasses/response_classes.py
@@ -1,16 +1,24 @@
 from jsonschema import validate
 from src.schemas.post import POST_SCHEMA
+from src.enums.global_enums import GlobalErrorMesages
 
 
 class ResponseClass:
     def __init__(self, response):
         self.response = response
+        self.response_json = response.json()
+        self.response_status = response.status_code
 
     def validate(self, schema):
-        if isinstance(self.response, list):
-            for item in self.response:
+        if isinstance(self.response_json, list):
+            for item in self.response_json:
                 validate(item, schema)
         else:
-            validate(self.response, schema)
+            validate(self.response_json, schema)
 
-
+    def assert_status_code(self, status_code):
+        if isinstance(status_code, list):
+            assert self.response_status in status_code, GlobalErrorMesages.WRONG_STATUS_CODE.value
+        else:
+            assert self.response_status == status_code, GlobalErrorMesages.WRONG_STATUS_CODE.value
+        return self
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,28 +1,14 @@
 import requests
 import conftest
 
-from src.enums.global_enums import GlobalErrorMesages
+from src.baseclasses.response_classes import ResponseClass
 from src.schemas.post import POST_SCHEMA
-from jsonschema import validate
-
-
-# def test_equal():
-#     assert 1 == 1, "Number is not equal to expected"
 
 
 def test_getting_posts():
-    response = requests.get(url=conftest.SERVICE_URL)
-    received_posts = response.json()
-    # print(received_posts)
+    res = requests.get(url=conftest.SERVICE_URL)
+    response = ResponseClass(res)
 
-    assert response.status_code == 200, GlobalErrorMesages.WRONG_STATUS_CODE.value
-    assert len(received_posts) == 3, GlobalErrorMesages.WRONG_ELEMENT_COUNT.value
-    """
-    Moved to response_classes.py
-    
-    for item in received_posts:
-        validate(item, POST_SCHEMA)
-    
-    """
+    response.assert_status_code(200).validate(POST_SCHEMA)
 
 
