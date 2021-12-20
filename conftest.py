59dcfb49c8b32efd0ceafa4efb2ed892e96a7660
iwebnal <iwebnal@gmail.com>
2021-12-21 00:35:20 +0300

    Add get_users fixture

--- conftest.py
+++ conftest.py
@@ -1,2 +1,12 @@
+import pytest
+import requests
+
 SERVICE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
 SERVICE_URL_1 = "https://gorest.co.in/public/v1/users"
+
+
+@pytest.fixture
+def get_users():
+    response = requests.get(SERVICE_URL_1)
+    # print('hallo')
+    return response
--- tests/test_2.py
+++ tests/test_2.py
@@ -1,15 +1,17 @@
 import requests
 
-from conftest import SERVICE_URL_1
+# from conftest import SERVICE_URL_1
 from src.baseclasses.response_classes import ResponseClass
 from src.schemas.users import User
 
-resp = requests.get(SERVICE_URL_1)
+# resp = requests.get(SERVICE_URL_1)
 
 
-def test_getting_users_list():
-    response = requests.get(SERVICE_URL_1)
-    test_obj = ResponseClass(response)
-    test_obj.assert_status_code(200).validate(User)
+def test_getting_users_list(get_users):
+    # response = requests.get(SERVICE_URL_1)
+    # test_obj = ResponseClass(get_users)
+    # test_obj.assert_status_code(200).validate(User)
+
+    ResponseClass(get_users).assert_status_code(200).validate(User)
 
 # print(resp.json())
